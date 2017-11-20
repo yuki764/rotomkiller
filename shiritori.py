96cab0d4372e60a6ba0f5663e3960b6f0d9ae3c8
Setuu <setuu@neigepluie.net>
2017-11-20 17:37:39 +0900

    change auto mode
     choice a candidate whose the number of next candidates is the smallest.

--- shiritori.py
+++ shiritori.py
@@ -36,6 +36,16 @@ shiri_filter = {
         '2':'ツ','Z':'ト','♂':'ス','♀':'ス'
 }
 
+def get_candidates_list(query):
+    candidates_list = []
+    shiri_pos = len(query) - 1
+    if(query[shiri_pos] == 'ー'):
+        shiri_pos = shiri_pos - 1
+    for pokemon in pokemon_list:
+        if(shiri_filter[query[shiri_pos]] == shiri_filter[pokemon[0]] and pokemon[len(pokemon) - 1] != 'ン' and used_pokemon[pokemon] == False):
+            candidates_list.append(pokemon)
+    return candidates_list
+
 # create list and map
 with open('pokemon_list.csv') as plf:
     for item in plf:
@@ -50,21 +60,31 @@ while(True):
         used_pokemon[query] = True
 
         # get candidates
-        candidates_list = []
-        shiri_pos = len(query) - 1
-        if(query[shiri_pos] == 'ー'):
-            shiri_pos = shiri_pos - 1
-        # print the "shiri" character
-        #print('next is "{}"'.format(query[shiri_pos]))
-        for pokemon in pokemon_list:
-            if(shiri_filter[query[shiri_pos]] == shiri_filter[pokemon[0]] and pokemon[len(pokemon) - 1] != 'ン' and used_pokemon[pokemon] == False):
-                candidates_list.append(pokemon)
+        candidates_list = get_candidates_list(query)
+
         # auto mode enabled
         if(len(sys.argv) == 2):
+            # if there are no candidates, you lose...
             if(len(candidates_list) == 0):
                 print('You lose...')
                 sys.exit(0)
-            choice = candidates_list[random.randint(0, len(candidates_list) - 1)]
+
+            # get indices of min of the # of next candidates
+            next_candidates_num = []
+            for candidate in candidates_list:
+                next_candidates_list = get_candidates_list(candidate)
+                next_candidates_num.append(len(next_candidates_list))
+                # print a list of candidates
+                #print('* {}: {}'.format(candidate, len(next_candidates_list)))
+
+            min_next_candidates_num = min(next_candidates_num)
+            min_next_candidates_indices = []
+            for i in range(0, len(next_candidates_num)):
+                if(next_candidates_num[i] == min_next_candidates_num):
+                    min_next_candidates_indices.append(i)
+
+            # choice randomly in indices of min
+            choice = candidates_list[random.choice(min_next_candidates_indices)]
             used_pokemon[choice] = True
             print('You: {}'.format(choice))
         # auto mode disabled
@@ -75,7 +95,7 @@ while(True):
             # input player's choice
             while(True):
                 choice = input('You: ')
-                if(choice == 'exit' or choice == 'surrender'):
+                if(choice == 'exit' or choice == 'surrender' or choice == 'コウサン'):
                     print('You lose...')
                     sys.exit(0)
                 elif(choice in candidates_list):
@@ -85,7 +105,7 @@ while(True):
                 else:
                     print('error: This isn\'t a name of Pokemon and in a candidates list.')
 
-    elif(query == 'exit' or query == 'surrender'):
+    elif(query == 'exit' or query == 'surrender' or query == 'コウサン'):
         print('You win!')
         break
     else:
